5d71e4179c3beedef15c9d632a5c8142734777dc
Ameya Keskar <ameyakeskar1999@gmail.com>
2021-02-07 16:54:22 +0530

    implemented basic ETA calculator

--- lib/components/navigate_component.dart
+++ lib/components/navigate_component.dart
@@ -9,7 +9,11 @@ class NavigateComponent extends StatefulWidget {
 
 class _NavigateComponentState extends State<NavigateComponent> {
   AutoCompleteTextField searchTextField;
+  AutoCompleteTextField searchTextField1;
   GlobalKey<AutoCompleteTextFieldState<Building>> key = new GlobalKey();
+  GlobalKey<AutoCompleteTextFieldState<Building>> key1 = new GlobalKey();
+
+  var displayETA = false;
 
   Widget row(Building building) {
     return Row(
@@ -69,7 +73,7 @@ class _NavigateComponentState extends State<NavigateComponent> {
                       children: [
                         Icon(Icons.fiber_manual_record, color: Colors.grey),
                         SizedBox(
-                            width: 300.0,
+                            width: 336.0,
                             child: searchTextField =
                                 AutoCompleteTextField<Building>(
                               itemSubmitted: (item) => setState(() =>
@@ -87,7 +91,7 @@ class _NavigateComponentState extends State<NavigateComponent> {
                                     right: 10.0,
                                     bottom: 20.0,
                                   ),
-                                  hintText: 'Search Location...',
+                                  hintText: 'From Location',
                                   hintStyle: TextStyle(
                                     color: Colors.black,
                                   )),
@@ -105,7 +109,89 @@ class _NavigateComponentState extends State<NavigateComponent> {
                               clearOnSubmit: false,
                             )),
                       ]),
-                )
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0, bottom: 10.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.fiber_manual_record,
+                            color: Colors.grey[900]),
+                        SizedBox(
+                            width: 336.0,
+                            child: searchTextField1 =
+                                AutoCompleteTextField<Building>(
+                              itemSubmitted: (item) => setState(() =>
+                                  searchTextField1.textField.controller.text =
+                                      item.name),
+                              key: key1,
+                              suggestions: buildings,
+                              style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                              decoration: InputDecoration(
+                                  contentPadding: EdgeInsets.only(
+                                    left: 10.0,
+                                    top: 30.0,
+                                    right: 10.0,
+                                    bottom: 20.0,
+                                  ),
+                                  hintText: 'To Destination',
+                                  hintStyle: TextStyle(
+                                    color: Colors.black,
+                                  )),
+                              itemBuilder: (context, item) {
+                                return row(item);
+                              },
+                              itemSorter: (x, y) {
+                                return x.name.compareTo(y.name);
+                              },
+                              itemFilter: (item, query) {
+                                return item.name
+                                    .toLowerCase()
+                                    .contains(query.toLowerCase());
+                              },
+                              clearOnSubmit: false,
+                            )),
+                      ]),
+                ),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  FlatButton(
+                    onPressed: () {
+                      double t = 0.0;
+                      double t1 = 0.0;
+
+                      buildings.forEach((element) {
+                        if (element.name ==
+                            searchTextField.textField.controller.text) {
+                          t = element.eta;
+                        }
+                        if (element.name ==
+                            searchTextField1.textField.controller.text) {
+                          t1 = element.eta;
+                        }
+                      });
+                      print(t1 - t);
+                    },
+                    child: Text('Calculate Time'),
+                    textColor: Colors.grey,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.grey)),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  RaisedButton(
+                    onPressed: null,
+                    child:
+                        Text('Navigate', style: TextStyle(color: Colors.white)),
+                    color: Colors.grey,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.grey)),
+                  ),
+                ]),
               ],
             )
           ],
